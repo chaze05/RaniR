e0679abcfa0536a317d74ea72e3e6598710ac086
Rani M. Rubillos <ranirubillos05@gmail.com>
2024-11-22 09:45:48 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
